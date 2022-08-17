c9804d7bc89c1cc0e2199dea08cdb23da6db7336
vinic <vinic@DESKTOP-MFQL7AO>
2022-08-17 16:59:46 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Model/ToDoList.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Model
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+    }
+}
